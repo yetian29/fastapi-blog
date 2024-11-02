35c9e68270472b4ee5d605ae454a76365826f101
yetian29 <dathien000@gmail.com>
2024-11-02 07:55:56 +0700

    Initial

--- src/api/v1/post/schemas.py
+++ src/api/v1/post/schemas.py
@@ -12,19 +12,21 @@ class PostInSchema(BaseModel):
 
     @field_validator("title")
     def validate_title(cls, value: str) -> str:
+        value = value.strip()
         if len(value) > 128:
             raise ValueError("Title must be less than 128 characters.")
         return value
 
     @field_validator("content")
     def validate_content(cls, value: str) -> str:
+        value = value.strip()
         if len(value) > 1024:
             raise ValueError("Content must be less than 1024 characters.")
         return value
 
     @model_validator(mode="after")
     def check_title_or_content(self) -> "PostInSchema":
-        if self.title == "" or self.content == "":
+        if not self.title or not self.content:
             raise ValueError("Invalid. Title and Content are required.")
         return self
 
